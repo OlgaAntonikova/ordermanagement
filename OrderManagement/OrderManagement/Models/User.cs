a0dde89850fa40da7236a17f09818c7ec699e5c8
Olga Antonikova <Olga.Antonikova@tecan.com>
2025-09-25 16:49:22 +0200

    Basic app without user authentication: - React UI connected to ASP.NET Core API - Orders creation and order status updation implemented - SQLite database in Data folder

---
+++ OrderManagement/OrderManagement/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManagement.Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public long TelegramId { get; set; }        
+        public string? Username { get; set; }
+        public long? ChatId { get; set; }
+        public List<Order> Orders { get; set; } = new();
+    }
+}
